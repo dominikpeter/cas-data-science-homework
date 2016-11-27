1ed9b6fd072bd7fad908212d8457229f8c6dea21
dominikpeter <dominik.peter87@gmail.com>
2016-11-27 16:18:41 +0100

    plotting

--- stats/Aufgabe 6/Lösung_Lorenzkurve.R
+++ stats/Aufgabe 6/Lösung_Lorenzkurve.R
@@ -22,8 +22,21 @@ Lcx <- Lc(markt.daten)
 # --------------------------------------------------------------------------------------------------
 
 plot(Lcx, main = "Lorenzkurve",
+     col = "red",
+     lty = 5,
+     lwd = 1,
      xlab=expression(u[i]),
-     ylab = expression(v[i]))
+     ylab = expression(v[i]),
+     xaxt="n",
+     yaxt="n")
+
+seq <- seq(0,1, by = 0.2)
+labels <- paste0(seq * 100, "%")
+
+axis(side = 1, at = seq, labels = labels)
+axis(side = 2, at = seq, labels = labels)
+grid()
+
 
 
 # Berechnen Sie den normalen Ginikoeffizienten
