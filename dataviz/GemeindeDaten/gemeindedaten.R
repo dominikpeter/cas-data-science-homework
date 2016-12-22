ed66a8b9e6b0f6ceda35421a0505121fac7532ac
dominikpeter <dominik.peter87@gmail.com>
2016-12-22 17:38:55 +0100

    push

--- dataviz/GemeindeDaten/gemeindedaten.R
+++ dataviz/GemeindeDaten/gemeindedaten.R
@@ -1,5 +1,16 @@
 
 rm(list=ls())
 library(data.table)
+library(magrittr)
 
-x <- fread("dataviz/GemeindeDaten/gemeindedaten.csv")
+df <- fread("dataviz/GemeindeDaten/gemeindedaten.csv")
+
+df %>% setkey(sprachregionen)
+df[, rank := frank(-bev_total, ties.method = "dense"), by = key(df)]
+df[rank == 1, .(bfsid,
+                gmdename,
+                kantone,
+                grossregionen,
+                sprachregionen,
+                stadt_land)]
+
