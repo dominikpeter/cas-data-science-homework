01bca655c404872389d8ec281cf75bd510213562
dominikpeter <dominik.peter87@gmail.com>
2016-12-22 20:52:18 +0100

    commit

--- dataviz/GemeindeDaten/gemeindedaten.R
+++ dataviz/GemeindeDaten/gemeindedaten.R
@@ -7,10 +7,7 @@ df <- fread("dataviz/GemeindeDaten/gemeindedaten.csv")
 
 df %>% setkey(sprachregionen)
 df[, rank := frank(-bev_total, ties.method = "dense"), by = key(df)]
-df[rank == 1, .(bfsid,
-                gmdename,
-                kantone,
-                grossregionen,
-                sprachregionen,
-                stadt_land)]
+df[rank == 1, .(bfsid, gmdename,
+                kantone, grossregionen,
+                sprachregionen, stadt_land)]
 
