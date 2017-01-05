b3148b05f4050ca466f24502743d2f0cbf832872
dominikpeter <dominik.peter87@gmail.com>
2017-01-05 12:41:59 +0100

    new

--- dataviz/GemeindeDaten/gemeindedaten_dplyr_vs_data.table.R
+++ dataviz/GemeindeDaten/gemeindedaten_dplyr_vs_data.table.R
@@ -13,7 +13,7 @@ df <- fread(path)
 
 df %>% setkey(sprachregionen)
 df[, rank := frank(-bev_total, ties.method = "dense"), by = key(df)]
-df[rank == 1, .(bfsid, gmdename,
+df <- df[rank == 1, .(bfsid, gmdename,
                 kantone, grossregionen,
                 sprachregionen, stadt_land)]
 
@@ -27,17 +27,23 @@ library(tidyverse)
 
 start <- Sys.time()
 
-df <- read_csv(path)
+df2 <- read_csv(path)
 
-df <- df %>% 
+df2 <- df2 %>% 
   group_by(sprachregionen) %>% 
   mutate(rank = dense_rank(-bev_total)) %>% 
   filter(rank == 1) %>% 
-  select(bfsid, gmdename,kantone, grossregionen,sprachregionen, stadt_land)
+  select(bfsid,
+         gmdename,
+         kantone,
+         grossregionen,
+         sprachregionen,
+         stadt_land)
 
 end <- Sys.time()
 
-time_2 <- (end - start) %>% .[[1]] %>%  as.numeric
+time_2 <- (end - start)[[1]] %>%  as.numeric
 
 
-
+# -----------------------------------------------------
+# Data.table ist schneller. Jedoch ist dplyr der lesbarere Cod
