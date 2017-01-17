7dbdf8c69452c213a5f15fccd681257ab53a8cc4
dominikpeter <dominik.peter87@gmail.com>
2017-01-17 19:25:27 +0100

    new

--- stats/Aufgabe 9/Loesung_X.R
+++ stats/Aufgabe 9/Loesung_X.R
@@ -11,21 +11,20 @@ library(MASS)
 
 
 # ------------------------------------------------------------------------------------------------
-# Aufgabe: Linksseitiger Test bei μ, σ bekannt
+# Aufgabe: Anpassungstests
 # ------------------------------------------------------------------------------------------------
-# Problem: Die Datei „lightbulbs.txt“ enthält eine neue Stichprobe des Glühbirnenherstellers.
-# Laden Sie die Datei mit dem Befehl scan. Lässt sich aufgrund dieser Stichprobe die Behauptung des Herstellers,
-# dass die Glühbirnen eine Mindestlebensdauer von 10′000 Stunden besitzen, bei einem Signifikanzniveau von 1% verwerfen?
-# Die Standardabweichung beträgt 120 Stunden.
-
+# Problem: Die Unileitung vermutet folgendes Rauchverhalten ihrer Studierenden.
+# Heavy Never Occassionaly Regular 
+# 4.5% 79.5% 8.5% 7.5%
+# Prüfen Sie, ob die Stichprobe aus survey sich mit dieser Behauptung verträgt.
+# Bestimmen Sie den p-Wert, ohne auf die Funktion chisq.test zurückzugreifen.
 
 o <- as.numeric(table(survey$Smoke))
-n <- length(survey$Smoke)
+n <- length(na.omit(survey$Smoke))
 e <- c(0.045, 0.795, 0.085, 0.075) * n
 
-chi <- sum(((o-e)^2)/e)
-dof <- n - 1
+chi <- sum((o-e)^2/e)
+dof <- length(table(survey$Smoke))-1
 pchisq(chi, df = dof, lower.tail = FALSE)
-
 
 
